d5d8fe86d080f5850eba5e6a88a0bd08bbdf75fd
Ahmad F. Iksan <ahmad.f.iksan@gmail.com>
2023-07-11 14:38:22 +0700

    Initial Commit

---
+++ SpeechToText/SpeechToTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeechToTextApp.swift
+//  SpeechToText
+//
+//  Created by Ahmad Fadly Iksan on 11/07/23.
+//
+
+import SwiftUI
+
+@main
+struct SpeechToTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
